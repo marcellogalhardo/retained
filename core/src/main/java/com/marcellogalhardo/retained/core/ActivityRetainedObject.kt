db8e44de6e853a5b5908e48828f5f3703a0b1a76
Marcello Galhardo <marcello.galhardo@n26.com>
2021-01-02 13:21:22 +0100

    Fix ActivityRetainedObject.kt auto-refactor

--- core/src/main/java/com/marcellogalhardo/retained/core/ActivityRetainedObject.kt
+++ core/src/main/java/com/marcellogalhardo/retained/core/ActivityRetainedObject.kt
@@ -7,4 +7,4 @@ inline fun <reified T : Any> ComponentActivity.retain(
     key: String = T::class.java.name,
     defaultArgs: Bundle? = intent?.extras,
     noinline createRetainedObject: RetainedContext.() -> T
-): Lazy<T> = lazy { retain(key, owner = this, defaultArgs, createRetainedObject) }
+): Lazy<T> = lazy { retainObject(key, owner = this, defaultArgs, createRetainedObject) }
