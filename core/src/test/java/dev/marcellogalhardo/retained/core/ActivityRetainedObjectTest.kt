4f4dc23093bc55bfb63602061baf79f400ed16d6
Marcello Galhardo <marcello.galhardo@n26.com>
2021-03-29 14:28:26 +0200

    Improve Test Coverage

--- core/src/test/java/dev/marcellogalhardo/retained/core/ActivityRetainedObjectTest.kt
+++ core/src/test/java/dev/marcellogalhardo/retained/core/ActivityRetainedObjectTest.kt
@@ -1,5 +1,6 @@
 package dev.marcellogalhardo.retained.core
 
+import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.launchActivity
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
@@ -24,13 +25,54 @@ internal class ActivityRetainedObjectTest {
             }
         }
     }
+
+    @Test
+    fun `should request correct 'key' when creating a retained object`() {
+        launchActivity<EmptyActivity>().apply {
+            onActivity { sut ->
+                val vm by sut.retain { entry -> CounterViewModel(entry) }
+
+                assertThat(vm.entry.key).isEqualTo(CounterViewModel::class.java.name)
+            }
+        }
+    }
+
+    @Test
+    fun `should request correct 'classRef' when creating a retained object`() {
+        launchActivity<EmptyActivity>().apply {
+            onActivity { sut ->
+                val vm by sut.retain { entry -> CounterViewModel(entry) }
+
+                assertThat(vm.entry.classRef).isEqualTo(CounterViewModel::class)
+            }
+        }
+    }
+
+    @Test
+    fun `should call 'onClearedListeners' when scope is destroyed`() {
+        launchActivity<EmptyActivity>().apply {
+            var vm: Lazy<CounterViewModel>? = null
+            onActivity { sut ->
+                vm = sut.retain { entry -> CounterViewModel(entry) }
+            }
+            assertThat(vm?.value?.isCleared).isFalse()
+            moveToState(Lifecycle.State.DESTROYED)
+            assertThat(vm?.value?.isCleared).isTrue()
+        }
+    }
 }
 
-internal class CounterViewModel(val entry: RetainedEntry) {
+internal class CounterViewModel(val entry: RetainedEntry) : OnClearedListener {
+
+    var isCleared: Boolean = false
 
     var count: Int
         get() = entry.savedStateHandle.get("count") ?: 0
         set(value) {
             entry.savedStateHandle.set("count", value)
         }
+
+    override fun onCleared() {
+        isCleared = true
+    }
 }
--- fragment/src/test/java/dev/marcellogalhardo/retained/fragment/FragmentRetainedObjectTest.kt
+++ fragment/src/test/java/dev/marcellogalhardo/retained/fragment/FragmentRetainedObjectTest.kt
@@ -2,8 +2,10 @@ package dev.marcellogalhardo.retained.fragment
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth.assertThat
+import dev.marcellogalhardo.retained.core.OnClearedListener
 import dev.marcellogalhardo.retained.core.RetainedEntry
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -14,33 +16,76 @@ internal class FragmentRetainedObjectTest {
     @Test
     fun `should retain object when owner is recreated`() {
         launchFragmentInContainer { CounterFragment() }.apply {
-            onFragment { fragment ->
-                fragment.vm.count += 1
-                fragment.vmInActivity.count += 3
+            onFragment { sut ->
+                val vm by sut.retain { entry -> CounterViewModel(entry) }
+                val vmInActivity by sut.retainInActivity { entry -> CounterViewModel(entry) }
+
+                vm.count += 1
+                vmInActivity.count += 3
             }
             recreate()
-            onFragment { fragment ->
-                assertThat(fragment.vm.count).isEqualTo(1)
-                assertThat(fragment.vmInActivity.count).isEqualTo(3)
+            onFragment { sut ->
+                val vm by sut.retain { entry -> CounterViewModel(entry) }
+                val vmInActivity by sut.retainInActivity { entry -> CounterViewModel(entry) }
+                val vmInParent by sut.retainInParent { entry -> CounterViewModel(entry) }
+
+                assertThat(vm.count).isEqualTo(1)
+                assertThat(vmInActivity.count).isEqualTo(3)
                 // As this fragment does not contain a 'parentFragment', the parent is the Activity
-                assertThat(fragment.vmInParent.count).isEqualTo(fragment.vmInActivity.count)
+                assertThat(vmInParent.count).isEqualTo(vmInActivity.count)
             }
+        }
+    }
+
+    @Test
+    fun `should request correct 'key' when creating a retained object`() {
+        launchFragmentInContainer { CounterFragment() }.apply {
+            onFragment { sut ->
+                val vm by sut.retain { entry -> CounterViewModel(entry) }
+
+                assertThat(vm.entry.key).isEqualTo(CounterViewModel::class.java.name)
+            }
+        }
+    }
+
+    @Test
+    fun `should request correct 'classRef' when creating a retained object`() {
+        launchFragmentInContainer { CounterFragment() }.apply {
+            onFragment { sut ->
+                val vm by sut.retain { entry -> CounterViewModel(entry) }
+
+                assertThat(vm.entry.classRef).isEqualTo(CounterViewModel::class)
+            }
+        }
+    }
+
+    @Test
+    fun `should call 'onClearedListeners' when scope is destroyed`() {
+        launchFragmentInContainer { CounterFragment() }.apply {
+            var vm: Lazy<CounterViewModel>? = null
+            onFragment { sut ->
+                vm = sut.retain { entry -> CounterViewModel(entry) }
+            }
+            assertThat(vm?.value?.isCleared).isFalse()
+            moveToState(Lifecycle.State.DESTROYED)
+            assertThat(vm?.value?.isCleared).isTrue()
         }
     }
 }
 
-internal class CounterViewModel(val entry: RetainedEntry) {
+internal class CounterViewModel(val entry: RetainedEntry) : OnClearedListener {
+
+    var isCleared: Boolean = false
 
     var count: Int
         get() = entry.savedStateHandle.get("count") ?: 0
         set(value) {
             entry.savedStateHandle.set("count", value)
         }
+
+    override fun onCleared() {
+        isCleared = true
+    }
 }
 
-internal class CounterFragment : Fragment() {
-
-    val vm by retain { entry -> CounterViewModel(entry) }
-    val vmInParent by retainInParent { entry -> CounterViewModel(entry) }
-    val vmInActivity by retainInActivity { entry -> CounterViewModel(entry) }
-}
+internal class CounterFragment : Fragment()
