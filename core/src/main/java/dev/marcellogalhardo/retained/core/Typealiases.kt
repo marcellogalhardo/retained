ce28f7d257bcb17329950a640d8d1d432d288af4
Marcello Galhardo <marcello.galhardo@gmail.com>
2021-06-24 19:44:34 +0200

    Remove unused imports from Typealiases.kt

--- core/src/main/java/dev/marcellogalhardo/retained/core/Typealiases.kt
+++ core/src/main/java/dev/marcellogalhardo/retained/core/Typealiases.kt
@@ -1,11 +1,6 @@
 package dev.marcellogalhardo.retained.core
 
 import android.os.Bundle
-import androidx.lifecycle.ViewModelStoreOwner
-import androidx.savedstate.SavedStateRegistryOwner
 
 public typealias FindDefaultArgs = () -> Bundle?
-
 public typealias FindParametrizedDefaultArgs<T> = (T) -> Bundle?
-
-public typealias InstantiateRetained<T> = (RetainedEntry) -> T
