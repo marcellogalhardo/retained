e468be7e1afaef46e202c08f572c0f1bac10a6c1
Marcello Galhardo <marcello.galhardo@gmail.com>
2022-12-14 21:51:05 +0000

    Suppress warnings from `SampleView`

--- sample/src/main/java/dev/marcellogalhardo/retained/sample/SampleView.kt
+++ sample/src/main/java/dev/marcellogalhardo/retained/sample/SampleView.kt
@@ -1,18 +1,22 @@
 package dev.marcellogalhardo.retained.sample
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.findFragment
+import dev.marcellogalhardo.retained.core.ExperimentalRetainedApi
 import dev.marcellogalhardo.retained.view.retain
 
+@OptIn(ExperimentalRetainedApi::class)
 class SampleView(context: Context, attrs: AttributeSet) : AppCompatTextView(context, attrs) {
 
     private val presenter: SimplePresenter by retain {
         SimplePresenter()
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onAttachedToWindow() {
         super.onAttachedToWindow()
         val hostName = resolveHostName()
