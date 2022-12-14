744779d05fa29e516c17609cc79ff4bb47ba4926
Marcello Galhardo <marcello.galhardo@gmail.com>
2022-12-14 21:47:05 +0000

    Suppress warnings from `SampleFragment`

--- sample/src/main/java/dev/marcellogalhardo/retained/sample/SampleFragment.kt
+++ sample/src/main/java/dev/marcellogalhardo/retained/sample/SampleFragment.kt
@@ -1,5 +1,6 @@
 package dev.marcellogalhardo.retained.sample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -22,6 +23,7 @@ class SampleFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_sample, container)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<TextView>(R.id.fragmentTextView).apply {
