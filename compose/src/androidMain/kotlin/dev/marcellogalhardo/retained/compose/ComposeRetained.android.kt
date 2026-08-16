f6c2400a7129a1715d85c8055ca12c87613ada5f
Marcello Galhardo <mgalhardo@google.com>
2026-08-16 13:26:45 +0200

    Convert compose module to Kotlin Multiplatform
    Move ComposeRetained to commonMain source set and retainInActivity extension to androidMain. Apply kotlin-multiplatform plugin to compose build script.
    
    Test: ./gradlew formatKotlin apiDump apiCheck test

--- compose/src/androidMain/kotlin/dev/marcellogalhardo/retained/compose/ComposeRetained.android.kt
+++ compose/src/androidMain/kotlin/dev/marcellogalhardo/retained/compose/ComposeRetained.android.kt
@@ -7,43 +7,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisallowComposableCalls
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
-import androidx.lifecycle.ViewModelStoreOwner
-import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import dev.marcellogalhardo.retained.core.ExperimentalRetainedApi
-import dev.marcellogalhardo.retained.core.InternalRetainedApi
 import dev.marcellogalhardo.retained.core.RetainedEntry
-import dev.marcellogalhardo.retained.core.retain
-
-/**
- * Returns an existing retained instance of [T] or creates a new one in the scope (ie, [ComponentActivity]).
- *
- * ```
- * @Composable
- * fun MyComposable() {
- *     val vm = retain { ViewModel() }
- * }
- * class ViewModel(val name: String = "")
- * ```
- *
- * @see retain
- */
-@OptIn(InternalRetainedApi::class)
-@Composable
-public inline fun <reified T : Any> retain(
-    owner: ViewModelStoreOwner =
-        checkNotNull(LocalViewModelStoreOwner.current) {
-            "No ViewModelStoreOwner was provided via LocalViewModelStoreOwner"
-        },
-    key: String = T::class.java.name,
-    noinline instantiate: @DisallowComposableCalls (RetainedEntry) -> T,
-): T =
-    remember(key1 = key) {
-        retain(
-            key = key,
-            findOwner = { owner },
-            instantiate = instantiate,
-        ).value
-    }
 
 /**
  * Returns an existing retained instance of [T] scoped to this [ComponentActivity]:
@@ -61,7 +26,7 @@ public inline fun <reified T : Any> retain(
 @ExperimentalRetainedApi
 @Composable
 public inline fun <reified T : Any> retainInActivity(
-    key: String = T::class.java.name,
+    key: String = T::class.qualifiedName ?: T::class.simpleName ?: "RetainedInstance",
     noinline instantiate: @DisallowComposableCalls (RetainedEntry) -> T,
 ): T {
     val context = LocalContext.current
